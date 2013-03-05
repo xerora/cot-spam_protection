ff473299bb566598fe4ff466d59aa20ba94f42d3
xerora <tyler@xaez.org>
2013-03-05 10:02:08 -0800

    fixed forum section last poster updating

--- spam_protection/spam_protection.forums.newtopic.first.php
+++ spam_protection/spam_protection.forums.newtopic.first.php
@@ -14,14 +14,15 @@ if((bool)$cfg['plugin']['spam_protection']['filter_forums'])
 	$sp_post_text_length = mb_strlen(cot_import('rmsgtext','P','HTM'));
 	// Override updating last poster section information
 	$sp_topicmode = (cot_import('rtopicmode','P','BOL') && $cfg['forums']['cat_' . $s]['allowprvtopics']) ? 1 : 0;
-	if (($sp_topic_title_length > $cfg['forums']['mintitlelength']) &&
-		($sp_post_text_length > $cfg['forums']['minpostlength']))
+	if (($sp_topic_title_length >= $cfg['forums']['mintitlelength']) &&
+		($sp_post_text_length >= $cfg['forums']['minpostlength']))
 	{
 		// Ensure there won't be an error before changing this. Else it will
 		// always mark the topic as private if an error is hit.
 		$cfg['forums']['cat_' . $s]['allowprvtopics'] = 1;
 		$_POST['rtopicmode'] = 1;
 		$cfg['forums']['cat_' . $s]['countposts'] = 0;
+		$cache = FALSE;
 	}
 }
 ?>
